e5c7217f38144aeff810f1858f5de918e913c51d
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-10-19 19:01:19 +0300

    Initial commit

--- task/t5/speech_to_speech.py
+++ task/t5/speech_to_speech.py
@@ -6,7 +6,7 @@ import requests
 
 from task.constants import OPENAI_HOST, OPENAI_API_KEY
 
-# https://platform.openai.com/docs/guides/audio?example=audio-in
+# https://platform.openai.com/docs/guides/audio?example=audio-in#add-audio-to-your-existing-application
 
 #TODO:
 # You need to generate answer in audio format based on the audio message:
@@ -16,7 +16,7 @@ from task.constants import OPENAI_HOST, OPENAI_API_KEY
 # ---
 # Hints:
 #   - Use /v1/chat/completions endpoint
-#   - Use gpt-4o-mini-tts model
+#   - Use gpt-4o-audio-preview model
 #   - Use modalities=["text", "audio"]
 #   - Use audio={"voice": "ballad", "format": "mp3"}
 #   - Similar method to encode audio https://platform.openai.com/docs/guides/images-vision?api-mode=chat&lang=python
